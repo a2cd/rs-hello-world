0eb5a7a498db11c6931c5150cfded686f4dea5a7
Kai <ca2pa23187@gmail.com>
2024-01-09 20:41:46 +0800

    feat: github ci

--- tests/lifetime_test.rs
+++ tests/lifetime_test.rs
@@ -1,13 +1,17 @@
 mod common;
+
 #[cfg(test)]
 mod lifetime_test {
     use crate::common;
     #[test]
     #[ignore]
-    #[should_panic]
     fn test_longest() {
         common::setup();
         assert_eq!(2+2, 4);
+    }
+
+    #[should_panic]
+    fn should_panic() {
         panic!("hhh");
     }
 }
