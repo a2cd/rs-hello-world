287c7e64cdb7865113272785f5fc2454750d8327
Kai <ca2pa23187@gmail.com>
2024-01-10 01:23:15 +0800

    refactor: 移除docker-compose端口

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rs_hello_world::api::controller::*;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
+    // 127无法外部访问
     let addrs = ("0.0.0.0", 8080);
     HttpServer::new(|| App::new().service(say_hello).service(print_index))
         .bind(addrs)?
