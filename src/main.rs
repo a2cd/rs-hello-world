5776fec7bd93f739b5ccf690e29767d53a357f41
Kai <ca2pa23187@gmail.com>
2024-01-10 00:43:19 +0800

    refactor: 开放0.0.0.0访问

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rs_hello_world::api::controller::*;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    let addrs = ("127.0.0.1", 8080);
+    let addrs = ("0.0.0.0", 8080);
     HttpServer::new(|| App::new().service(say_hello).service(print_index))
         .bind(addrs)?
         .run().await
