86042bcd73967d01731abcf42a9662463960d72f
Kai <ca2pa23187@gmail.com>
2024-01-10 22:34:56 +0800

    feat: cd

--- src/api/controller/hello.rs
+++ src/api/controller/hello.rs
@@ -4,3 +4,8 @@ use actix_web::{get, HttpResponse, Responder};
 pub async fn say_hello() -> impl Responder {
     HttpResponse::Ok().body("hello")
 }
+
+#[get("/hello-world")]
+pub async fn say_hello_world() -> impl Responder {
+    HttpResponse::Ok().body("hello world")
+}
