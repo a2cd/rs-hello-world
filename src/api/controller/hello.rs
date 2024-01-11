b8d7c2a21163d5e646e6b62f908bd90f4a25158c
Kai <ca2pa23187@gmail.com>
2024-01-11 21:12:18 +0800

    feat: hello time

--- src/api/controller/hello.rs
+++ src/api/controller/hello.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, HttpResponse, Responder};
+use chrono::{Local};
 
 #[get("/hello")]
 pub async fn say_hello() -> impl Responder {
@@ -8,4 +9,10 @@ pub async fn say_hello() -> impl Responder {
 #[get("/hello-world")]
 pub async fn say_hello_world() -> impl Responder {
     HttpResponse::Ok().body("hello world")
+}
+
+#[get("/hello/time")]
+pub async fn hello_time() -> impl Responder {
+    let now = Local::now().format("%Y-%m-%d %H:%M:%S");
+    HttpResponse::Ok().body(now.to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::Result;
 use actix_web::{App, HttpServer};
 use rs_hello_world::api::controller::*;
-use rs_hello_world::api::controller::hello::say_hello_world;
+use rs_hello_world::api::controller::hello::*;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
@@ -10,6 +10,7 @@ async fn main() -> Result<()> {
     HttpServer::new(|| App::new()
         .service(say_hello)
         .service(say_hello_world)
+        .service(hello_time)
         .service(print_index)
     ).bind(addrs)?.run().await
 }
