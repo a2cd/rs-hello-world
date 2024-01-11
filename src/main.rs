8f7c23b7cc8bcea316569c2a3ceefecc471b3e8b
Kai <ca2pa23187@gmail.com>
2024-01-11 20:59:33 +0800

    feat: cd

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::io::Result;
 use actix_web::{App, HttpServer};
 use rs_hello_world::api::controller::*;
+use rs_hello_world::api::controller::hello::say_hello_world;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
     // 127无法外部访问
     let addrs = ("0.0.0.0", 8080);
-    HttpServer::new(|| App::new().service(say_hello).service(print_index))
-        .bind(addrs)?
-        .run().await
+    HttpServer::new(|| App::new()
+        .service(say_hello)
+        .service(say_hello_world)
+        .service(print_index)
+    ).bind(addrs)?.run().await
 }
 
 
